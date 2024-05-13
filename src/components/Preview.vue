4180909af7cafe33e3240f27e6dee43f93301a53
Lorenzo Vannucci <lore.ucci@gmail.com>
2024-05-13 22:29:17 +0200

    Read server address from environment

--- src/components/Preview.vue
+++ src/components/Preview.vue
@@ -10,7 +10,7 @@ function getPreviewSize() {
 const imgurl = ref('')
 
 onMounted(async () => {
-    const res = await fetch("http://localhost:3000/albums/" + props.album_id + "/thumbnail/" + props.photo_id + "?size=" + props.preview_size)
+    const res = await fetch(import.meta.env.VITE_MEMORIA_SERVER + "/albums/" + props.album_id + "/thumbnail/" + props.photo_id + "?size=" + props.preview_size)
     imgurl.value = "data:image/jpg;base64," + (await res.json()).imgdata
 })
 </script>
